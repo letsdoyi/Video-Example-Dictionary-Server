14e2fae751d7fe86ee78eee7e1a358e7b65a9c9f
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-12-04 21:13:00 +0900

    Added URLs

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,8 @@
 module.exports = {
-  CLIENT_URL: 'https://getsample.netlify.com/',
+  CLIENT_URL: {
+    PRODUCTION: 'https://getsample.netlify.com',
+    DEVELOPMENT: 'http://localhost:3000'
+  },
   GOOGLE_API_SCOPE: {
     PLUS: 'https://www.googleapis.com/auth/plus.login',
     YOUTUBE: 'https://www.googleapis.com/youtube/v3'

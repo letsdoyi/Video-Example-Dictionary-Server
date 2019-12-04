dd9bb854733858e63654963976a721a98c5e0217
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-12-04 20:11:06 +0900

    Changed Client Url to getsample.netlify.com

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CLIENT_URL: 'http://localhost:3000/',
+  CLIENT_URL: 'https://getsample.netlify.com/',
   GOOGLE_API_SCOPE: {
     PLUS: 'https://www.googleapis.com/auth/plus.login',
     YOUTUBE: 'https://www.googleapis.com/youtube/v3'

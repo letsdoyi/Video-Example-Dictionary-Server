04ae594c8f95fee8b655566f205de857dd0bedc4
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-12-04 20:40:15 +0900

    Bugfixed about cors

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,13 @@
+// Connect Dotenv
+if (process.env.NODE_ENV === 'development') {
+  const dotenv = require('dotenv');
+  dotenv.config({
+    path: './.env',
+  });
+}
+
 module.exports = {
-  CLIENT_URL: 'http://localhost:3000/',
+  CLIENT_URL: process.env.CLIENT_URL,
   GOOGLE_API_SCOPE: {
     PLUS: 'https://www.googleapis.com/auth/plus.login',
     YOUTUBE: 'https://www.googleapis.com/youtube/v3'
